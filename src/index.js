97f4a07e59495149c2773027b3624778e8d75902
mcmhav <mcmhav@gmail.com>
2017-11-21 23:45:13 +0100

    return image?

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 const express = require('express')
+const fs = require('fs')
 const { exec } = require('child_process')
 const randomstring = require('randomstring')
 
 const app = express()
 
-const PORT = 3000
+const PORT = 80
+
+const takeImage = () => {
+  return new Promise((resolve, reject) => {
+    const imgName = randomstring.generate(7)
+    exec(`raspistill -o ${imgName}.jpg`, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        reject()
+        return
+      }
+
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`)
+      console.log(`stderr: ${stderr}`)
+
+      resolve(imgName)
+    })
+  })
+}
 
 app.get('/', (req, res) => {
-  const imgName = randomstring.generate(7)
-  exec(`raspistill -o ${imgName}.jpg`, (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      return
-    }
+  try {
+    const imageName = await takeImage();
 
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`)
-    console.log(`stderr: ${stderr}`)
-  })
-
-  return res.send('Hello World!')
+    const fileToLoad = fs.readFileSync(file);
+    res.writeHead(200, {'Content-Type':  contentType });
+    res.end(fileToLoad, 'binary');
+  } catch (e) {
+    return res.send('ajjajj')
+  }
 })
 
 app.listen(PORT, () =>
